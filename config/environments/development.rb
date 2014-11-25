1fd53f5e12911ea4461ffcec9fc1d89164d4373e
pjurczynski <piotr.jurczynski@netguru.co>
2014-11-25 12:38:49 +0100

    use env.fetch [#83384746]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options =
-    { host: ENV['DOMAIN_NAME'] || 'localhost:3000' }
+    { host: ENV.fetch('DOMAIN_NAME') { 'localhost:3000' } }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 end
--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,14 +1,14 @@
 require 'rollbar/rails'
 
 Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_SERVER_TOKEN']
-  config.exception_level_filters.merge!(
-     'ActionController::RoutingError' => 'ignore'
-   )
-
   if Rails.env.test? || Rails.env.development?
     config.enabled = false
-  end
+  else
+    config.access_token = ENV.fetch('ROLLBAR_SERVER_TOKEN') { 'not_valid_token' }
+    config.exception_level_filters.merge!(
+      'ActionController::RoutingError' => 'ignore'
+    )
 
-  config.person_username_method = "email"
+    config.person_username_method = 'email'
+  end
 end
