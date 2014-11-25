ef24022d277ec8da8f6a334dcb773c4c39175118
pjurczynski <piotr.jurczynski@netguru.co>
2014-11-25 12:39:17 +0100

    init api [#83384746]

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,27 +1,7 @@
 class API < Grape::API
-  format :json
-  formatter :json, Grape::Formatter::ActiveModelSerializers
+  use APILogger
   logger Rails.logger
   def self.logger
     @logger ||= Rails.logger
   end
-
-  helpers do
-    include SessionsHelper
-
-    def invalid_login_attempt
-      error!('Error with your login or password', 401)
-    end
-
-    def current_user
-      @current_user ||= validate_session!(params[:sessionToken])
-    end
-  end
-
-  use APILogger
-  mount V1::Base
-
-  add_swagger_documentation(
-    api_version: 'v1'
-  )
 end
