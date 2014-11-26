9c5632f57e25257cbd5adcab7dad21f6298e220d
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-11-26 12:45:39 +0100

    cleanup application controller [#83384746]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
-
-  decent_configuration do
-    strategy DecentExposure::StrongParametersStrategy
-  end
-
-  def current_user
-    @current_user = nil
-  end
 end
