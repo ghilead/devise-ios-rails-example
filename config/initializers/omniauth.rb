16ebbce806087a5c9f14d44421ca23891d7a7362
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-11-27 10:11:16 +0100

    remove not used omniauth [#83533280]

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-    Rails.application.secrets.google_client_id,
-    Rails.application.secrets.google_secret,
-    scope: 'email,profile',
-    prompt: :select_account
-end
-
