c8973c1e7d0114c254f7e9a7b4e5edef3d707444
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-12-01 12:00:39 +0100

    add code and status to error object [#83558622]

--- app/models/v1/error.rb
+++ app/models/v1/error.rb
@@ -1,7 +1,9 @@
 module V1
   class Error
-    def initialize(message: nil)
+    def initialize(message: nil, code: 0, status:)
       @message = message
+      @code = code
+      @status = status
     end
   end
 end
