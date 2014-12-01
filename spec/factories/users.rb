8c5913f80a1874344c32a327f6a483a433a33d0c
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-12-01 17:53:40 +0100

    add full trait for users
    [#83465828]

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :user do
     email 'ios_user@example.com'
     password 'alcatraz'
+
+    trait :full do
+      reset_password_token { Devise.token_generator.generate(User, :reset_password_token) }
+      reset_password_sent_at { Time.now }
+      remember_created_at { Time.now }
+    end
   end
 end
