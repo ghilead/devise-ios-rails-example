80999dcc558e1b18190b50dc213de0989b8bef82
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-12-04 15:37:59 +0100

    hide format in documentation
    [#83914540]

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,3 +1,4 @@
 GrapeSwaggerRails.options.url = '/swagger_doc.json'
 GrapeSwaggerRails.options.app_name = 'Saaskit'
 GrapeSwaggerRails.options.app_url = ENV.fetch('DOMAIN_URL')
+GrapeSwaggerRails.options.hide_format = true
