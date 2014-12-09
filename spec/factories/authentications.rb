8eb2d851292b9c79ce84650460dcf16113d10b73
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-12-09 11:51:50 +0100

    add authentication factory [#84148458]

---
+++ spec/factories/authentications.rb
@@ -0,0 +1,47 @@
+class AuthenticationFactory
+  attr_reader :user_token, :user_email
+
+  def initialize(user_token:, user_email:)
+    @user_token = user_token
+    @user_email = user_email
+  end
+
+  def valid?
+    true
+  end
+
+  def to_hash
+    {
+      userToken: user_token,
+      userEmail: user_email,
+    }
+  end
+end
+
+FactoryGirl.define do
+  factory :authentication, class: AuthenticationFactory do
+    skip_create
+
+    transient do
+      user
+    end
+
+    initialize_with do
+      user_token ||= user.authentication_token
+      user_email ||= user.email
+      new(
+        user_token: user_token,
+        user_email: user_email,
+      )
+    end
+  end
+
+  factory :invalid_authentication, class: AuthenticationFactory do
+    initialize_with do
+      new(
+        user_token: 'non existent',
+        user_email: 'no_such@example.com',
+      )
+    end
+  end
+end
